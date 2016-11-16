51b47d0cb7324b039c9100db08714a58bab0afa5
Victor Puigcerver <vicpuigcer@gmail.com>
2016-11-16 18:51:39 +0100

    Hamburger + Fragments Working

---
+++ app/src/main/java/com/example/admin/sdosandroidcars/GalleryFragment.java
@@ -0,0 +1,29 @@
+package com.example.admin.sdosandroidcars;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.admin.sdosandroidcars.R;
+
+/**
+ * Created by Admin on 16/11/2016.
+ */
+
+public class GalleryFragment extends Fragment {
+
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        //returning our layout file
+        //change R.layout.yourlayoutfilename for each of your fragments
+        return inflater.inflate(R.layout.frag_gallery, container, false);
+    }
+
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        //you can set the title for your toolbar here for different fragments different titles
+        getActivity().setTitle("Gallery");
+    }
+}
