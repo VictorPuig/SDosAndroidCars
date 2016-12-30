85b4f6e2ba279e4db0afb43da747f8a4b3eaec08
Victor Puigcerver <vicpuigcer@gmail.com>
2016-12-30 13:12:06 +0100

    Afegida funcionalitat per afegir nou cotxe amb foto i tot (la foto es puja al servidor)

--- app/src/main/java/com/example/admin/sdosandroidcars/Fragments/AddCarFragment.java
+++ app/src/main/java/com/example/admin/sdosandroidcars/Fragments/AddCarFragment.java
@@ -1,9 +1,17 @@
 package com.example.admin.sdosandroidcars.fragments;
 
 import android.content.DialogInterface;
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AlertDialog;
+import android.util.Base64;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -26,10 +34,19 @@ import com.example.admin.sdosandroidcars.api.info.Filter;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.ByteArrayOutputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import static android.app.Activity.RESULT_OK;
+
 public class AddCarFragment extends BaseFragment implements View.OnClickListener, FilterAvailableListener {
 
+    private static final String TAG = "AddCarFragment";
     Spinner makerSpinner, colorSpinner;
     AddCarFragment self = this;
+    private String base64Img;
+    private static final int PICK_IMAGE = 1;
 
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         //returning our layout file
@@ -43,23 +60,74 @@ public class AddCarFragment extends BaseFragment implements View.OnClickListener
         getActivity().setTitle("AddCar");
 
         Button btnOk = (Button) getView().findViewById(R.id.buttonAddCarsOk);
+        Button selectImg = (Button) getView().findViewById(R.id.buttonSelectImg);
+
         makerSpinner = (Spinner) getView().findViewById(R.id.makerSpinner);
         colorSpinner = (Spinner) getView().findViewById(R.id.colorSpinner);
-        TextView textViewNewMaker = (TextView) getView().findViewById(R.id.textViewNewMaker);
-        TextView textViewNewColor = (TextView) getView().findViewById(R.id.textViewNewColor);
+
+        Button btnNewMaker = (Button) getView().findViewById(R.id.btnNewMaker);
+        Button btnNewColor = (Button) getView().findViewById(R.id.btnNewColor);
 
         ((Drawer) getActivity()).getFilter(this);
 
         btnOk.setOnClickListener(this);
-        textViewNewColor.setOnClickListener(this);
-        textViewNewMaker.setOnClickListener(this);
+        selectImg.setOnClickListener(this);
+        btnNewMaker.setOnClickListener(this);
+        btnNewColor.setOnClickListener(this);
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == PICK_IMAGE && resultCode == RESULT_OK
+                && null != data) {
+
+            Uri selectedImage = data.getData();
+            Log.d(TAG, "selectedImage = " + selectedImage);
+            String[] filePathColumn = { MediaStore.Images.Media.DATA };
+
+            Cursor cursor = getContext().getContentResolver().query(selectedImage,
+                    filePathColumn, null, null, null);
+
+
+            int columnIndex = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+            cursor.moveToFirst();
+
+            BitmapFactory.Options options = new BitmapFactory.Options();
+            options.inPreferredConfig = Bitmap.Config.ARGB_8888;
+
+            Bitmap bitmap = null;
+            try {
+                bitmap = MediaStore.Images.Media.getBitmap(getContext().getContentResolver(), selectedImage);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            base64Img = getEncoded64ImageStringFromBitmap(bitmap);
+
+            cursor.close();
+
+        }
+    }
+
+    public String getEncoded64ImageStringFromBitmap(Bitmap bitmap) {
+        ByteArrayOutputStream stream = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 70, stream);
+        byte[] byteFormat = stream.toByteArray();
+        // get the base 64 string
+        String imgString = Base64.encodeToString(byteFormat, Base64.NO_WRAP);
+
+        return imgString;
     }
 
     @Override
     public void onClick(View view) {
         int id = view.getId();
 
-        if (id == R.id.buttonAddCarsOk) {
+        if (id == R.id.buttonSelectImg) {
+            selectImageFromGallery();
+        }
+
+        else if (id == R.id.buttonAddCarsOk) {
             EditText editText = (EditText) getView().findViewById(R.id.modelName);
 
             final String modelName = editText.getText().toString();
@@ -83,7 +151,7 @@ public class AddCarFragment extends BaseFragment implements View.OnClickListener
                         public void onClick(DialogInterface dialogInterface, int i) {
                             Toast.makeText(getContext(), "Sending...", Toast.LENGTH_SHORT).show();
 
-                            Car2 newCar = new Car2(modelName, color, maker);
+                            Car2 newCar = new Car2(modelName, color, maker, base64Img);
 
                             Cars.doAddCar(newCar.getJSONObject(), new CarsResultListener() {
                                 @Override
@@ -97,7 +165,7 @@ public class AddCarFragment extends BaseFragment implements View.OnClickListener
                     .show();
 
 
-        } else if (id == R.id.textViewNewMaker) {
+        } else if (id == R.id.btnNewMaker) {
             final EditText editTextNewMaker = new EditText(getContext());
 
             new AlertDialog.Builder(getContext())
@@ -136,7 +204,7 @@ public class AddCarFragment extends BaseFragment implements View.OnClickListener
                     })
                     .show();
 
-        } else if (id == R.id.textViewNewColor) {
+        } else if (id == R.id.btnNewColor) {
             final EditText editTextNewColor = new EditText(getContext());
 
             new AlertDialog.Builder(getContext())
@@ -177,6 +245,13 @@ public class AddCarFragment extends BaseFragment implements View.OnClickListener
         }
     }
 
+    public void selectImageFromGallery() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent, "Select Picture"),PICK_IMAGE);
+    }
+
     @Override
     public void onFilterAvailable(Filter filter) {
         ElementSpinnerAdapter makerAdapter = new ElementSpinnerAdapter(getContext(), android.R.layout.simple_spinner_dropdown_item, filter.getMakers());
@@ -185,5 +260,8 @@ public class AddCarFragment extends BaseFragment implements View.OnClickListener
         ElementSpinnerAdapter colorAdapter = new ElementSpinnerAdapter(getContext(), android.R.layout.simple_spinner_dropdown_item, filter.getColors());
         colorSpinner.setAdapter(colorAdapter);
     }
+
+
+
 }
 
--- app/src/main/java/com/example/admin/sdosandroidcars/api/cars/Car2.java
+++ app/src/main/java/com/example/admin/sdosandroidcars/api/cars/Car2.java
@@ -10,11 +10,13 @@ public class Car2 {
     private final Element maker;
     private final Element color;
     private final String name;
+    private final String img;
 
-    public Car2(String name, Element color, Element maker) {
+    public Car2(String name, Element color, Element maker, String img) {
         this.name = name;
         this.color = color;
         this.maker = maker;
+        this.img = img;
     }
 
     public JSONObject getJSONObject() {
@@ -24,6 +26,7 @@ public class Car2 {
             carObj.put("name", name);
             carObj.put("maker", maker.getJSONObject());
             carObj.put("color", color.getJSONObject());
+            carObj.put("img", img);
         } catch (JSONException e) {
             e.printStackTrace();
         }
