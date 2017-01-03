75b410593fd3de8c5b044ebf6420925253935345
Guillem Cruz <gcquiroga@gmail.com>
2017-01-03 14:02:58 +0100

    Afegida la funcionalitat de CarDetail (canviar per un fragment)

--- app/src/main/java/com/example/admin/sdosandroidcars/adapters/GridViewAdapter.java
+++ app/src/main/java/com/example/admin/sdosandroidcars/adapters/GridViewAdapter.java
@@ -1,24 +1,20 @@
 package com.example.admin.sdosandroidcars.adapters;
 
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.Point;
 import android.support.annotation.NonNull;
-import android.util.Log;
-import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.ViewTreeObserver;
 import android.view.WindowManager;
 import android.widget.ArrayAdapter;
-import android.widget.ImageView;
 import android.widget.PopupWindow;
-import android.widget.TextView;
 
+import com.example.admin.sdosandroidcars.CarDetail;
 import com.example.admin.sdosandroidcars.Drawer;
 import com.example.admin.sdosandroidcars.R;
 import com.example.admin.sdosandroidcars.api.cars.Car;
-import com.example.admin.sdosandroidcars.utils.StringUtils;
 import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
@@ -66,47 +62,13 @@ public class GridViewAdapter extends ArrayAdapter<Car> {
         gridViewItem.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+            Intent intent = new Intent(context, CarDetail.class);
+            intent.putExtra("maker", getItem(position).getMaker());
+            intent.putExtra("model", getItem(position).getName());
+            intent.putExtra("color", getItem(position).getColor());
+            intent.putExtra("url", getItem(position).getImgUrl());
+            context.startActivity(intent);
 
-                Log.d("a ver", "click");
-
-                if (popup != null) {
-                    Log.d("a ver", "cancelant");
-
-                    popup.dismiss();
-                    popup = null;
-
-                    return;
-                }
-
-                final View popUpView = layoutInflater.inflate(R.layout.car_detail, null);
-
-                popup = new PopupWindow(popUpView, ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT, false);
-
-                ImageView imageViewCar = (ImageView) popUpView.findViewById(R.id.imageViewCar);
-                imageViewCar.setImageDrawable(gridViewItem.getDrawable());
-
-                TextView textViewModel = (TextView) popUpView.findViewById(R.id.textViewCarModel);
-                textViewModel.setText(StringUtils.titleCase(getItem(position).getName()));
-
-                TextView textViewMaker = (TextView) popUpView.findViewById(R.id.textViewCarMaker);
-                textViewMaker.setText(StringUtils.titleCase(getItem(position).getMaker()));
-
-                TextView textViewColor = (TextView) popUpView.findViewById(R.id.textViewCarColor);
-                textViewColor.setText(StringUtils.titleCase(getItem(position).getColor()));
-
-                popup.setOutsideTouchable(true);
-
-                popUpView.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
-                    @Override
-                    public void onGlobalLayout() {
-                        int x = (p.x / 2) - popUpView.getMeasuredWidth() / 2;
-                        int y = (p.y / 2) - popUpView.getMeasuredHeight() / 2 + toolbarHeight / 2;
-
-                        popup.update(x, y, -1, -1);
-                    }
-                });
-
-                popup.showAtLocation(parent, Gravity.NO_GRAVITY, 0, 0);
             }
         });
 
